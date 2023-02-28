9bded624f1635534ff26040132213b38d6139c4b
Monimrowka <116653626+Monimrowka@users.noreply.github.com>
2023-02-28 15:05:06 +0100

    Landing Page

--- slimteam-frontend/src/components/Navbar/Navbar.js
+++ slimteam-frontend/src/components/Navbar/Navbar.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import './navbar.css'
-import { NavLink} from 'react-router-dom';
+import React from "react";
+import "./navbar.css";
+import { NavLink } from "react-router-dom";
 import axios from "axios";
-import Button from '@mui/material/Button';
-import LogoCompany from './header.png'
+import Button from "@mui/material/Button";
+// import LogoCompany from './header.png'
+import LogoCompany from "./SlimTeam.jpg";
 
-
-
-function Navbar({userLogin, setUserLogin,name}) {
-  console.log(name)
+function Navbar({ userLogin, setUserLogin, name }) {
+  console.log(name);
 
   const logoutOnClick = (e) => {
     axios
@@ -22,31 +21,67 @@ function Navbar({userLogin, setUserLogin,name}) {
           console.log(`Error logging out the user ${err}`);
         }
       });
-      setUserLogin(false)
+    setUserLogin(false);
   };
-console.log(`User login status: ${userLogin}`)
+  console.log(`User login status: ${userLogin}`);
   return (
     <>
-     <div className='container-header'>
-        <p><img className='img-header' src={LogoCompany} alt="logo-coffeeMeet"/></p>
-    
-      <div className='row container-navbar'>
-        <ul className='col-8 lists-navbar'>
-          <NavLink to="/" className='items-navbar'>About us</NavLink> |
-          <NavLink to="/explore-experts" className='items-navbar'>Explore experts</NavLink> |
-          <NavLink to="/meet-us" className='items-navbar'>Meet us</NavLink> |
-          <NavLink to="/jobwall" className='items-navbar'>JobWall</NavLink> 
-        </ul>
-        <ul className='col-4 login-navbar'>
+      <div className="container-header">
+        <p>
+          <img className="img-header" src={LogoCompany} alt="logo-coffeeMeet" />
+        </p>
 
-        <Button variant="contained"><NavLink to={`${userLogin ? `/portfolio/${name}` : '/register'}` } className='items-buttons register'>{userLogin ? 'Profile' : 'Register' }</NavLink></Button> |
-        <Button variant="contained"><NavLink to={`${userLogin ? '/login' : '/login'}` } className='items-buttons'>{userLogin ? (<span onClick={logoutOnClick}>Logout</span>) : 'Login' }</NavLink></Button>
-
-        </ul>
+        <div className="row container-navbar">
+          <ul className="col-8 lists-navbar">
+            <NavLink to="/" className="items-navbar">
+              About us
+            </NavLink>{" "}
+            |
+            <NavLink to="/explore-experts" className="items-navbar">
+              Explore experts
+            </NavLink>{" "}
+            |
+            <NavLink to="/meet-us" className="items-navbar">
+              Meet us
+            </NavLink>{" "}
+            |
+            <NavLink to="/jobwall" className="items-navbar">
+              JobWall
+            </NavLink>
+          </ul>
+          <ul className="col-4 login-navbar">
+            <Button
+              variant="contained"
+              style={{ backgroundColor: "black" }}
+            >
+              <NavLink
+                to={`${userLogin ? `/portfolio/${name}` : "/register"}`}
+                className="items-buttons register"
+              >
+                {userLogin ? "Profile" : "Register"}
+              </NavLink>
+            </Button>{" "}
+            |
+            <Button
+              variant="contained"
+              style={{ backgroundColor: "black" }}
+            >
+              <NavLink
+                to={`${userLogin ? "/login" : "/login"}`}
+                className="items-buttons"
+              >
+                {userLogin ? (
+                  <span onClick={logoutOnClick}>Logout</span>
+                ) : (
+                  "Login"
+                )}
+              </NavLink>
+            </Button>
+          </ul>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
