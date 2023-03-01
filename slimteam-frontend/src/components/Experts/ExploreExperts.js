f173dab4a423328f7ee8a8f8348c99da8bd57bb0
Aroj1986 <116952156+Aroj1986@users.noreply.github.com>
2023-03-01 20:40:03 +0100

    final search and filter works fine

--- slimteam-frontend/src/components/Experts/ExploreExperts.js
+++ slimteam-frontend/src/components/Experts/ExploreExperts.js
@@ -13,14 +13,13 @@ import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions } from "@mui/material";
 
 export default function ExploreExperts({ experts, setExperts }) {
-
   // sort by country
   const [checkedCountry, setCheckedCountry] = useState([
     { count: "germany", checked: false },
     { count: "england", checked: false },
   ]);
   const [prevCountryChecked, setPrevCountryChecked] = useState(false);
-  const [sortedExpertsByCountry, setSortedExpertsByCountry] = useState(experts);
+  const [sortedExpertsByCountry, setSortedExpertsByCountry] = useState(false);
 
   // sort by expertise
   const [checkedExpertise, setCheckedExpertise] = useState([
@@ -28,12 +27,14 @@ export default function ExploreExperts({ experts, setExperts }) {
     { expertise: "Sheet metal", checked: false },
   ]);
   const [sortedExpertsByExpertise, setSortedExpertsByExpertise] =
-    useState(experts);
+    useState(false);
 
   const navigate = useNavigate();
 
   let selected = [];
 
+  console.log(sortedExpertsByCountry);
+  console.log(sortedExpertsByExpertise);
   return (
     <>
       <hr />
@@ -42,7 +43,7 @@ export default function ExploreExperts({ experts, setExperts }) {
           <div className="filter-container">
             <h6>
               <>
-              <SearchExpert experts={experts} setExperts={setExperts} /> 
+                <SearchExpert experts={experts} setExperts={setExperts} />
                 <h5>Filter your search results:</h5>
               </>
             </h6>
@@ -69,7 +70,11 @@ export default function ExploreExperts({ experts, setExperts }) {
         </div>
 
         <div className="col col-9">
-        <button onClick={() => navigate(0)} className="goBackArrow">
+          {experts.length ? (
+            <div>
+              {sortedExpertsByCountry.length ? (
+                <div>
+                  <button onClick={() => navigate(0)} className="goBackArrow">
                     <span>
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -85,10 +90,6 @@ export default function ExploreExperts({ experts, setExperts }) {
                     </span>
                     <span className="goBackText"></span>
                   </button>
-          {experts.length ? (
-            <div>
-              {sortedExpertsByCountry.length ? (
-                <div>
                   <div className="expert-list-container">
                     {sortedExpertsByCountry.map((expert, index) => (
                       <Card sx={{ maxWidth: 250 }}>
@@ -131,10 +132,31 @@ export default function ExploreExperts({ experts, setExperts }) {
                 <div>
                   {sortedExpertsByExpertise.length ? (
                     <div>
+                      <button
+                        onClick={() => navigate(0)}
+                        className="goBackArrow"
+                      >
+                        <span>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
+                            fill="gray"
+                            className="bi bi-skip-backward-btn "
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M11.21 5.093A.5.5 0 0 1 12 5.5v5a.5.5 0 0 1-.79.407L8.5 8.972V10.5a.5.5 0 0 1-.79.407L5 8.972V10.5a.5.5 0 0 1-1 0v-5a.5.5 0 0 1 1 0v1.528l2.71-1.935a.5.5 0 0 1 .79.407v1.528l2.71-1.935z" />
+                            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z" />
+                          </svg>
+                        </span>
+                        <span className="goBackText"></span>
+                      </button>
                       <div className="expert-list-container">
                         {sortedExpertsByExpertise.map((expert, index) => (
-                          <Card    border="primary"
-                          style={{ width: "15rem", height: "22rem" }}>
+                          <Card
+                            border="primary"
+                            style={{ width: "15rem", height: "22rem" }}
+                          >
                             <CardActionArea>
                               <CardMedia
                                 component="img"
@@ -174,11 +196,12 @@ export default function ExploreExperts({ experts, setExperts }) {
                       </div>
                     </div>
                   ) : (
-
                     <div className="expert-list-container">
                       {experts.map((expert, index) => (
-                        <Card    border="primary"
-                        style={{ width: "15rem", height: "22rem" }}>
+                        <Card
+                          border="primary"
+                          style={{ width: "15rem", height: "22rem" }}
+                        >
                           <CardActionArea>
                             <CardMedia
                               component="img"
