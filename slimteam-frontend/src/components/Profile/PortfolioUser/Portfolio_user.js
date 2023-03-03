9547749951f31426446236069b0b5be76321e87e
Sindhura <sindhura.kontham@gmail.com>
2023-03-03 23:18:01 +0100

    jsx fix

--- slimteam-frontend/src/components/Profile/PortfolioUser/Portfolio_user.js
+++ slimteam-frontend/src/components/Profile/PortfolioUser/Portfolio_user.js
@@ -31,8 +31,8 @@ export default function Portfolio_user({ name, email, setName }) {
 
   }, [name]);
 
-
-
+  console.log(portfolio)
+  
   return (
     <Container>
       {portfolio ? <div className="expert-portfolio">
@@ -48,70 +48,59 @@ export default function Portfolio_user({ name, email, setName }) {
               style={{ height: 150, width: 150 }}
             />
           </div>
-      <Card
-        style={{
-          backgroundColor: " rgba(255,250,250)",
-          padding: "0rem",
-          width: "272%",
-          borderRadius: "1rem",
-        }}
-      >
-        <CardActionArea>
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Details
-              <HeadlineEdit
-                className="edit-function"
-                id_expert={portfolio?._id}
-                name={name}
-                first_name={portfolio?.personal_details.first_name}
-                last_name={portfolio?.personal_details.last_name}
-                address={portfolio?.personal_details.address.street}
-                city={portfolio?.personal_details.address.city}
-                nationality={portfolio?.personal_details.nationality}
-                email={portfolio?.personal_details.email}
-                phone_number={portfolio?.personal_details.phone_number}
-                portfolio={portfolio}
-                setPortfolio={setPortfolio}
-                setName={setName}
-              />
-            </Typography>
-            <CardActions
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                marginTop: "1rem"
-              }}
-            >
-              <Button size="small" color="primary">
-                <NavLink to="/managebookings" className="button-expert">
-                  ManageBookings
-                </NavLink>
-              </Button>
-            </CardActions>
 
-            <Typography variant="body2" color="text.secondary" align="left">
-              <b>
-                Name: {portfolio?.personal_details.first_name}{" "}
-                {portfolio?.personal_details.last_name}
-              </b>
-            </Typography>
+          <Card
+            style={{
+              backgroundColor: " rgba(255,250,250)",
+              padding: "0rem",
+              width: "272%",
+              borderRadius: "1rem",
+            }}
+          >
+            <CardActionArea>
+              <CardContent>
+                <Typography className = "headlineeditbutton" gutterBottom variant="h5" component="div">
+                  PROFILE DETAILS
+                  <HeadlineEdit
+                    className="edit-function"
+                    id_expert={portfolio?._id}
+                    name={name}
+                    first_name={portfolio?.personal_details?.first_name}
+                    last_name={portfolio?.personal_details?.last_name}
+                    street={portfolio?.personal_details?.address?.street}
+                    city={portfolio?.personal_details?.address?.city}
+                    nationality={portfolio?.personal_details?.nationality}
+                    email={portfolio?.personal_details?.email}
+                    phone_number={portfolio?.personal_details?.phone_number}
+                    portfolio={portfolio}
+                    setPortfolio={setPortfolio}
+                    setName={setName}
+                  />
+                </Typography>
+                <CardActions
+                  style={{ display: "flex", justifyContent: "center" }}
+                >
+                </CardActions>
 
-            <Typography variant="body2" color="text.secondary" align="left">
-              <h6>Address: {portfolio?.personal_details.address.street} </h6>
-              <h6>City: {portfolio?.personal_details.address.city}</h6>
-              <h6>Country: {portfolio?.personal_details.nationality}</h6>
-              <h6>E-Mail: {portfolio?.personal_details.email} </h6>
-              <h6>
-                Phone number: {portfolio?.personal_details.phone_number}{" "}
-              </h6>
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    </div>
-  </div>
-</Container>
+                <Typography variant="body2" color="text.secondary" align="left">
+                  <b>
+                    {portfolio?.personal_details.first_name}{" "}
+                    {portfolio?.personal_details.last_name}
+                  </b>
+                </Typography>
+
+                <Typography variant="body2" color="text.secondary" align="left">
+                  <h6>
+                    Address: {portfolio?.personal_details.address.street}{" "}
+                  </h6>
+                  <h6>City: {portfolio?.personal_details.address.city}</h6>
+                  <h6>Country: {portfolio?.personal_details.nationality}</h6>
+                  <h6>E-Mail: {portfolio?.personal_details.email} </h6>
+                  <h6>
+                    Phone number: {portfolio?.personal_details.phone_number}{" "}
+                  </h6>
+                </Typography>
+
                 <CardActions
                   style={{ display: "flex", justifyContent: "center" }}
                 >
