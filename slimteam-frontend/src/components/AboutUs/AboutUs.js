50e400933e517463380bd11e3086101dacd66a0c
Monimrowka <116653626+Monimrowka@users.noreply.github.com>
2023-03-05 13:46:07 +0100

    icons landing page

--- slimteam-frontend/src/components/AboutUs/AboutUs.js
+++ slimteam-frontend/src/components/AboutUs/AboutUs.js
@@ -5,14 +5,13 @@ import arojPicture from "./ArojPicture.jpg";
 
 import HandymanIcon from "@mui/icons-material/Handyman";
 import ElectricBoltIcon from "@mui/icons-material/ElectricBolt";
-import CleaningServicesIcon from '@mui/icons-material/CleaningServices';
-import ImagesearchRollerIcon from '@mui/icons-material/ImagesearchRoller';
-import ChildFriendlyIcon from '@mui/icons-material/ChildFriendly';
+import CleaningServicesIcon from "@mui/icons-material/CleaningServices";
+import ImagesearchRollerIcon from "@mui/icons-material/ImagesearchRoller";
+import ChildFriendlyIcon from "@mui/icons-material/ChildFriendly";
 
 import logo from "../../images/logo.png";
 import { Button } from "@mui/material";
 import { NavLink } from "react-router-dom";
-
 
 function AboutUs() {
   return (
@@ -20,21 +19,25 @@ function AboutUs() {
       <div className="d-flex flex-column justify-content-center align-items-center mt-3 mb-3 ">
         <div className="d-flex flex-row aroj justify-content-md-between align-items-center p-2">
           <div className="hero-section">
-            <h2 className="slogan" style={{display:"inline"}}>Our Connection to Excellent <p style={{margin:"0.4rem",display:"inline"}}></p><img src={logo} height={70} width={60}></img> ervices</h2>
+            <h2 className="slogan" style={{ display: "inline" }}>
+              Our Connection to Excellent{" "}
+              <p style={{ margin: "0.4rem", display: "inline" }}></p>
+              <img src={logo} height={70} width={60}></img> ervices
+            </h2>
             <p className="paragraph">
-
-              From minor repairs to major renovations, our service experts have got you covered - 
+              From minor repairs to major renovations, our service experts have
+              got you covered -
               <Button size="small" color="primary">
-                              <NavLink
-                                to={`/explore-experts`}
-                                className="button-expert"
-                                style={{textDecoration:"none",backgroundColor:"black"} }
-                              >
-                                BOOK NOW
-                              </NavLink>
-                            </Button> <br />Receive your future staff for your job within a few
-              hours.
-
+                <NavLink
+                  to={`/explore-experts`}
+                  className="button-expert"
+                  style={{ textDecoration: "none", backgroundColor: "black" }}
+                >
+                  BOOK NOW
+                </NavLink>
+              </Button>{" "}
+              <br />
+              Receive your future staff for your job within a few hours.
             </p>
           </div>
           <img
@@ -45,8 +48,36 @@ function AboutUs() {
           />
         </div>
         <br></br>
+        <div className="reviews">
+          <div className="icons"> 
+          <ElectricBoltIcon style={{ color: "gold", fontSize: "2.5rem" }} />
+          <p>Electrician services</p>
+          </div>
 
-        <Carousel
+          <div className="icons"> 
+          <HandymanIcon style={{ color: "grey", fontSize: "2.5rem" }} />
+          <p>Plumbing services</p>
+        </div>
+
+        <div className="icons"> 
+          <CleaningServicesIcon style={{ color: "#654321", fontSize: "2.5rem" }} />
+              
+                <p>Cleaning services</p>
+        </div>
+
+        <div className="icons"> 
+        <ImagesearchRollerIcon style={{color: "#333333", fontSize: "2.5rem" }} />
+              
+                <p>Home Painting services</p>
+        </div>
+
+        <div className="icons"> 
+        <ChildFriendlyIcon style={{ color: "green", fontSize: "3rem" }}/>
+                <p>Nanny services</p>
+        </div>
+        </div>
+
+        {/* <Carousel
           variant="dark"
           className="flex-carousel text-center"
           style={{ width: 300 }}
@@ -109,7 +140,7 @@ function AboutUs() {
               </p>
             </div>
           </Carousel.Item>
-        </Carousel>
+        </Carousel> */}
         <br />
         <br />
       </div>
