d67bdc7204a1fd7ab8a2c9f91a4bbe75f01a8cab
Monimrowka <116653626+Monimrowka@users.noreply.github.com>
2023-03-06 17:25:58 +0100

    New version of landing page

--- slimteam-frontend/src/components/AboutUs/AboutUs.js
+++ slimteam-frontend/src/components/AboutUs/AboutUs.js
@@ -9,7 +9,7 @@ import ElectricBoltIcon from "@mui/icons-material/ElectricBolt";
 import CleaningServicesIcon from "@mui/icons-material/CleaningServices";
 import ImagesearchRollerIcon from "@mui/icons-material/ImagesearchRoller";
 import ChildFriendlyIcon from "@mui/icons-material/ChildFriendly";
-import CarpenterIcon from '@mui/icons-material/Carpenter';
+import CarpenterIcon from "@mui/icons-material/Carpenter";
 import logo from "../../images/logo.png";
 import { Button } from "@mui/material";
 import { NavLink } from "react-router-dom";
@@ -20,71 +20,82 @@ function AboutUs() {
       <div className="d-flex flex-column justify-content-center align-items-center mt-3 mb-3 ">
         <div className="d-flex flex-row aroj justify-content-md-between align-items-center p-2">
           <div className="hero-section">
-
-            <h3 className="slogan" style={{display:"inline"}}>Our Connection to Excellent 
-            
-            {/* <p style={{margin:"0.4rem",display:"inline"}}></p><img src={logo} height={70} width={60}></img>  */} Services</h3>
+            <h3 className="slogan" style={{ display: "inline" }}>
+              Our Connection to Excellent
+              {/* <p style={{margin:"0.4rem",display:"inline"}}></p><img src={logo} height={70} width={60}></img>  */}{" "}
+              Services
+            </h3>
             <p className="paragraph">
-
-              From minor repairs to major renovations, our service experts have got you covered.
-             
-                            <br /> <br />Receive your future staff for your job within a few
-              hours.
-              <br /> <br />
-
+              <p className="paratext">
+                From minor repairs to major renovations, our service experts
+                have got you covered.{" "}
+              </p>
+              <p className="paratext">Get access to more Skilled
+              Professionals or More Work by registering with our app!{" "}
             </p>
-            <Button size="small" color="primary" style={{textAlign:"center"}}>
-                              <NavLink
-                                to={`/explore-experts`}
-                                className="button-expert"
-                                style={{textDecoration:"none",backgroundColor:"black"} }
-                              >
-                                BOOK NOW
-                              </NavLink>
-                              </Button>
 
+            <p className="paratext">
+              Receive your future staff for your job within a few hours.{" "}
+            </p>
+           
+            </p>
+
+            <Button
+              variant="contained"
+              style={{ backgroundColor: "black", width: "9rem" }}
+            >
+              <NavLink
+                to={`/explore-experts`}
+                className="button-expert"
+                style={{ textDecoration: "none", backgroundColor: "black" }}
+              >
+                BOOK NOW
+              </NavLink>
+            </Button>
           </div>
           <img
             className="image"
             src={handymen}
             alt="arojPicture"
-            style={{ height: 280, width:500}}
+            style={{ width: 580 }}
           />
-
-        
         </div>
         <br></br>
         <div className="reviews">
-          <div className="icons"> 
-          <ElectricBoltIcon style={{ color: "gold", fontSize: "2.5rem" }} />
-          <p>Electrician services</p>
+          <div className="icons">
+            <ElectricBoltIcon style={{ color: "gold", fontSize: "2.5rem" }} />
+            <p>Electrician services</p>
           </div>
 
-          <div className="icons"> 
-          <HandymanIcon style={{ color: "grey", fontSize: "2.5rem" }} />
-          <p>Plumbing services</p>
-        </div>
+          <div className="icons">
+            <HandymanIcon style={{ color: "grey", fontSize: "2.5rem" }} />
+            <p>Plumbing services</p>
+          </div>
 
-        <div className="icons"> 
-          <CleaningServicesIcon style={{ color: "#654321", fontSize: "2.5rem" }} />
-              
-                <p>Cleaning services</p>
-        </div>
+          <div className="icons">
+            <CleaningServicesIcon
+              style={{ color: "#654321", fontSize: "2.5rem" }}
+            />
 
-        <div className="icons"> 
-        <ImagesearchRollerIcon style={{color: "#333333", fontSize: "2.5rem" }} />
-              
-                <p>Home Painting services</p>
-        </div>
+            <p>Cleaning services</p>
+          </div>
 
-        <div className="icons"> 
-        <ChildFriendlyIcon style={{ color: "green", fontSize: "3rem" }}/>
-                <p>Nanny services</p>
-        </div>
-        <div className="icons"> 
-        <CarpenterIcon style={{ color: "grey", fontSize: "3rem" }}/>
-                <p>Carpenter services</p>
-        </div>
+          <div className="icons">
+            <ImagesearchRollerIcon
+              style={{ color: "#333333", fontSize: "2.5rem" }}
+            />
+
+            <p>Home Painting services</p>
+          </div>
+
+          <div className="icons">
+            <ChildFriendlyIcon style={{ color: "green", fontSize: "3rem" }} />
+            <p>Nanny services</p>
+          </div>
+          <div className="icons">
+            <CarpenterIcon style={{ color: "grey", fontSize: "3rem" }} />
+            <p>Carpenter services</p>
+          </div>
         </div>
 
         {/* <Carousel
