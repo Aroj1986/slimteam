b6ca903f1c8b1b3374be010ba67d39d6137b965c
Aroj1986 <116952156+Aroj1986@users.noreply.github.com>
2023-03-08 12:21:02 +0100

    correct path for callbackfunctions

--- Routes/CalenderRoutes.js
+++ Routes/CalenderRoutes.js
@@ -1,16 +1,29 @@
-const express = require('express')
-const CalenderRoutes = express.Router()
-const {createAppointment,getAppointments,getBookingInfo,getexpertBookingInfo,getAllAppointments, getAppointment, updateAppointment, deleteAppointment,getDetailsUserExpert} = require('../CallbackFunctions/CalenderCallbackFunctions')
+const express = require("express");
+const CalenderRoutes = express.Router();
+const {
+  createAppointment,
+  getAppointments,
+  getBookingInfo,
+  getexpertBookingInfo,
+  getAllAppointments,
+  getAppointment,
+  updateAppointment,
+  deleteAppointment,
+  getDetailsUserExpert,
+} = require("../CallbackFunctions/calenderCallbackFunctions");
 
-
-CalenderRoutes.route('/book-online/:id').get(getAppointment).delete(deleteAppointment).put(updateAppointment)
-CalenderRoutes.route('/book-online').post(createAppointment).get(getAllAppointments)
-CalenderRoutes.route('/book-online/:name/:expertName').get(getAppointments)
-CalenderRoutes.route('/profile-details/:name').get(getDetailsUserExpert)
-CalenderRoutes.route('/managebookings/:name').get(getBookingInfo)
-CalenderRoutes.route('/manageexpertbookings/:name').get(getexpertBookingInfo)
+CalenderRoutes.route("/book-online/:id")
+  .get(getAppointment)
+  .delete(deleteAppointment)
+  .put(updateAppointment);
+CalenderRoutes.route("/book-online")
+  .post(createAppointment)
+  .get(getAllAppointments);
+CalenderRoutes.route("/book-online/:name/:expertName").get(getAppointments);
+CalenderRoutes.route("/profile-details/:name").get(getDetailsUserExpert);
+CalenderRoutes.route("/managebookings/:name").get(getBookingInfo);
+CalenderRoutes.route("/manageexpertbookings/:name").get(getexpertBookingInfo);
 
 // CalenderRoutes.route('/book-online/name/:name').get(getAppointment)
-
 
 module.exports = CalenderRoutes;
--- Routes/profilepic.js
+++ Routes/profilepic.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { createProfilePic } = require("../CallbackFunctions/profilePicCallbackFunction");
+const {
+  createProfilePic,
+} = require("../CallbackFunctions/profilePicCallbackFunction");
 // const { verifyToken } = require("../middlewares/verifyToken");
 
 const profilepicRouter = express.Router();
 
 profilepicRouter.put("/addprofilepic/:name", createProfilePic);
 
-module.exports = 
-    profilepicRouter
+module.exports = profilepicRouter;
